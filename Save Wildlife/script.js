a17696d36042c5503c46832aa6904bd2829d60a2
Angela Kang <48544188+kangelax@users.noreply.github.com>
2020-05-27 15:46:01 +1000

    Save Wildlife - Interactive Website

---
+++ Save Wildlife/script.js
@@ -0,0 +1,92 @@
+//audio will not autoplay
+audio.autoplay = false;
+
+//play kangaroo noise on click when paused, if playing then pause
+function playKangaroo() {
+var kangaroo = document.getElementById("kangarooAudio");
+    if (kangaroo.paused) {
+        kangaroo.play();
+    } else {
+        kangaroo.pause();
+    }
+}
+
+//play koala noise on click when paused, if playing then pause
+function playKoala() {
+    var koala = document.getElementById("koalaAudio");
+    if (koala.paused) {
+        koala.play();
+    } else {
+        koala.pause();
+    }
+}
+
+//play emu noise on click when paused, if playing then pause
+function playEmu() {
+    var emu = document.getElementById("emuAudio");
+    if (emu.paused) {
+        emu.play();
+    } else {
+        emu.pause();
+    }
+}
+
+//play platypus noise on click when paused, if playing then pause
+function playPlatypus() {
+    var platypus = document.getElementById("platypusAudio");
+    if (platypus.paused) {
+        platypus.play();
+    } else {
+        platypus.pause();
+    }
+}
+
+//play possum noise on click when paused, if playing then pause
+function playPossum() {
+    var possum = document.getElementById("possumAudio");
+    if (possum.paused) {
+        possum.play();
+    } else {
+        possum.pause();
+    }
+}
+
+//play wombat noise on click when paused, if playing then pause
+function playWombat() {
+    var wombat = document.getElementById("wombatAudio");
+    if (wombat.paused) {
+        wombat.play();
+    } else {
+        wombat.pause();
+    }
+}
+
+//play currawong noise on click when paused, if playing then pause
+function playCurrawong() {
+    var currawong = document.getElementById("currawongAudio");
+    if (currawong.paused) {
+        currawong.play();
+    } else {
+        currawong.pause();
+    }
+}
+
+//play cockatoo noise on click when paused, if playing then pause
+function playCockatoo() {
+    var cockatoo = document.getElementById("cockatooAudio");
+    if (cockatoo.paused) {
+        cockatoo.play();
+    } else {
+        cockatoo.pause();
+    }
+}
+
+//play kookaburra noise on click when paused, if playing then pause
+function playKookaburra() {
+    var kookaburra = document.getElementById("kookaburraAudio");
+    if (kookaburra.paused) {
+        kookaburra.play();
+    } else {
+        kookaburra.pause();
+    }
+}
